da763e6858a7c6ca0b8b8cbfbdebefa42ccafdaa
catherinezybareva <deadinsdeyouu@gmail.com>
2025-12-23 17:39:42 +0500

    Files were added

---
+++ server/application.py
@@ -0,0 +1,24 @@
+"""Simple HTTP server module for lab_cicd"""
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+class TestMe():
+   """Test class for demonstration purposes"""
+
+    def take_fife(self):
+        """Return the server port number"""
+        return 5
+
+    def port(self):
+        """Return the server port number"""
+        return PORT
+
+if __name__ == '__main__'
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as http:
+        print("serving at port", PORT)
+        http.server_forever()
